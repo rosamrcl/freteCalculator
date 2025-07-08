dc554a79e0da7eb4338f9aecf886ca9222c3defb
Rosa <costurezati@gmail.com>
2025-07-08 20:04:02 -0300

    .

--- FreeShippingCalculatorTest.php
+++ FreeShippingCalculatorTest.php
@@ -2,17 +2,17 @@
 
 class FreeShippingCalculatorTest{
 
-    public function ShouldApply_WhenValueisAboveMinimumTest(){
+    public function ShouldNotApply_WhenValueIsBelowMinimumTest(){
         $frete = new FreeShippingCalculator();
 
         $totalValue = 80;
         $totalValuefrete = $frete->apply(value:$totalValue);
 
         $expectedValue = false;
-        $this->asserEquals($expectedValue,$totalValuefrete);
+        $this->assertEquals($expectedValue,$totalValuefrete);
 
     }
-        public function asserEquals($expectedValue, $actualValue){
+        public function assertEquals($expectedValue, $actualValue){
             if ($expectedValue !==$actualValue){
                 $message = 'Expected: ' . $expectedValue . ' but got: ' . $actualValue;
                 throw new Exception(message: $message);
